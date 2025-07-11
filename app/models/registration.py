98d5b49c7023af0e574aec0f60093d09979b4f22
gcasu36 <g.casu36@studenti.unica.it>
2025-07-11 18:44:38 +0200

    Update registration.py

--- app/models/registration.py
+++ app/models/registration.py
@@ -1,8 +1,8 @@
 from sqlmodel import SQLModel, Field
 
 class Registration(SQLModel, table=True):
-    username: str = Field(primary_key=True, foreign_key="user.username", ondelete="CASCADE")
-    event_id: int = Field(primary_key=True, foreign_key="event.id", ondelete="CASCADE")
+    username: str = Field(primary_key=True, foreign_key="user.username", ondelete="CASCADE") # se l'utente viene eliminato, elimina anche le sue registrazioni
+    event_id: int = Field(primary_key=True, foreign_key="event.id", ondelete="CASCADE") # se l'evento viene eliminato, elimina anche le registrazioni collegate
 
 
 class RegistrationCreate(SQLModel):
