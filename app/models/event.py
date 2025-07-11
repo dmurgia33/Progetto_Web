b3e972d4cbb9293903060e9fbfa0a48061265307
gcasu36 <g.casu36@studenti.unica.it>
2025-07-11 18:42:55 +0200

    Update event.py

--- app/models/event.py
+++ app/models/event.py
@@ -1,10 +1,11 @@
 from sqlmodel import SQLModel, Field
-from datetime import datetime
 from typing import Optional
 
 
 class Event(SQLModel, table=True):
     __table_args__ = {'sqlite_autoincrement': True}
+    '''Senza sqlite_autoincrement, SQLite assegna il più piccolo ID disponibile. 
+    Con sqlite_autoincrement posto a True, SQLite continua sempre a incrementare l’ultimo ID massimo usato.'''
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     description: str
@@ -13,12 +14,14 @@ class Event(SQLModel, table=True):
 
 
 class EventCreate(SQLModel):
+    # Non include 'id' perché verrà generato automaticamente
     title: str
     description: str
     date: str
     location: str
 
 class EventRead(SQLModel):
+    # Include l'ID, che è noto solo dopo la creazione
     id: int
     title: str
     description: str
